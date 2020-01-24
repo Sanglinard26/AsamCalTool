98ffac1d427720dc0184af83f57e8f5ff4bd5a01
U354706 <U354706@B1029853.inetpsa.com>
2020-01-24 16:31:39 +0100

    24/01/2020 : test

--- A2LParser/src/gui/A2lDisplayer.java
+++ A2LParser/src/gui/A2lDisplayer.java
@@ -3,7 +3,10 @@
  */
 package gui;
 
+import java.awt.Adjustable;
 import java.awt.EventQueue;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 import java.io.File;
 import java.io.IOException;
 import java.nio.MappedByteBuffer;
@@ -15,17 +18,26 @@ import java.nio.file.StandardOpenOption;
 import java.util.concurrent.ExecutionException;
 
 import javax.swing.JFrame;
-import javax.swing.JTextPane;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingWorker;
 
 public final class A2lDisplayer extends JFrame {
 
     private static final String NAME = "C:\\User\\U354706\\Perso\\WorkInProgress\\64_00_42_(A520).a2l";
+    static int END_OF_TEXT = 10000;
 
     private static final long serialVersionUID = 1L;
 
     static SeekableByteChannel sbc = null;
     static MappedByteBuffer mappedByteBuffer = null;
+    static byte[] b;
+    static int fileLength;
+    static JTextArea textArea;
+    static JScrollPane scrollPane;
 
     private static class DisplayWorker extends SwingWorker<String, String> {
 
@@ -41,10 +53,14 @@ public final class A2lDisplayer extends JFrame {
 
             try {
                 sbc = Files.newByteChannel(file.toPath(), StandardOpenOption.READ);
+                fileLength = (int) sbc.size();
                 mappedByteBuffer = ((FileChannel) sbc).map(FileChannel.MapMode.READ_ONLY, 0, sbc.size());
 
-                byte[] b = new byte[(int) sbc.size()];
+                b = new byte[(int) sbc.size()];
                 mappedByteBuffer.get(b);
+
+                mappedByteBuffer.clear();
+                sbc.close();
 
                 return new String(b, 0, 10000, Charset.defaultCharset());
 
@@ -57,15 +73,68 @@ public final class A2lDisplayer extends JFrame {
 
         @Override
         protected void done() {
-            JTextPane jTextPane = new JTextPane();
             try {
-                jTextPane.setText(get());
+                textArea = new JTextArea(get(), 50, 100);
+                scrollPane = new JScrollPane(textArea, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+                        ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+                // scrollPane.setPreferredSize(new Dimension(1000, 500));
+                scrollPane.getVerticalScrollBar().addAdjustmentListener(new MyAdjustmentListener());
+                scrollPane.getHorizontalScrollBar().addAdjustmentListener(new MyAdjustmentListener());
+                JOptionPane.showMessageDialog(null, scrollPane);
             } catch (InterruptedException | ExecutionException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         }
 
+    }
+
+    static class MyAdjustmentListener implements AdjustmentListener {
+        public void adjustmentValueChanged(AdjustmentEvent evt) {
+            Adjustable source = evt.getAdjustable();
+            JScrollBar bar = (JScrollBar) source;
+            System.out.println("Bar min : " + bar.getMinimum());
+            System.out.println("Bar max : " + bar.getMaximum());
+            System.out.println("Bar value : " + bar.getValue());
+            if (evt.getValueIsAdjusting()) {
+            }
+            int orient = source.getOrientation();
+            if (orient == Adjustable.HORIZONTAL) {
+
+            } else {
+
+            }
+            int type = evt.getAdjustmentType();
+
+            switch (type) {
+            case AdjustmentEvent.UNIT_INCREMENT:
+
+                break;
+            case AdjustmentEvent.UNIT_DECREMENT:
+
+                break;
+            case AdjustmentEvent.BLOCK_INCREMENT:
+
+                break;
+            case AdjustmentEvent.BLOCK_DECREMENT:
+
+                break;
+            case AdjustmentEvent.TRACK:
+
+                break;
+            }
+            int value = evt.getValue();
+
+            if (value > 0) {
+                int offset = Math.min(0 + value * 10, fileLength - 1);
+                int length = Math.min(END_OF_TEXT + value * 10, fileLength - 1);
+                if (offset + length > fileLength - 1) {
+                    length = fileLength - 1 - offset;
+                }
+                // textArea.setText(new String(b, offset, length, Charset.defaultCharset()));
+
+            }
+
+        }
     }
 
     public static void main(String[] args) {
